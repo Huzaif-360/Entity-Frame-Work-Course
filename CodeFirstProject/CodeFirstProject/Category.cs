28e3400650d01d28b5e442dead969325887baf78
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-19 16:49:22 +0500

    Completed EFC Lecture 26  Migrations: Adding a New Class

---
+++ CodeFirstProject/CodeFirstProject/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeFirstProject
+{
+    //Lecture 26
+    //Migrations: Adding a New Class
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        /*Add a Migrations
+         * As already discussed that we shoudld add only small changes and then we should use migrations to make the database upto dated.
+         * 
+        */
+    }
+}
