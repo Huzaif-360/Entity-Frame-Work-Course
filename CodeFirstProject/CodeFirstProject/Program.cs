84db82b6bbeb1fabb553a751f3a71b3a8a414166
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-19 17:05:37 +0500

    Completed EFC Lecture 29 Migrations: Recovering from Mistakes

--- CodeFirstProject/CodeFirstProject/Program.cs
+++ CodeFirstProject/CodeFirstProject/Program.cs
@@ -4,8 +4,12 @@ using System.Data.Entity;
 
 namespace CodeFirst
 {
-    /*Completed Lecture 28
-     *  Migrations: Deleting an Existing Class
+    /*Completed Lecture 29
+     *  Migrations: Recovering From Mistake
+     *  
+     *  If we have deleted something which we should not delete. we can not recover unless we create a new migration and readd it.
+     *  so if we will delete the deleted migration from the folder of migration we can get stucked and we may not proceed further.
+     *  so only add a new migration and move forward
      *  
      *
      */
