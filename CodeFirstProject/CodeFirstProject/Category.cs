880adcc9e0aeeb6d08ec3ffc18187c86a077bb44
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-19 16:57:02 +0500

    Completed EFC Lecture 28 Migrations: Modifying an Existing Class

--- CodeFirstProject/CodeFirstProject/Category.cs
+++ CodeFirstProject/CodeFirstProject/Category.cs
@@ -4,16 +4,21 @@ using System.Text;
 
 namespace CodeFirstProject
 {
-    //Lecture 26
-    //Migrations: Adding a New Class
+    //Lecture 27
+    //Migrations: Modifying an Existing Class
     public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
         /*Add a Migrations
+         * Adding a new Property 
          * As already discussed that we shoudld add only small changes and then we should use migrations to make the database upto dated.
-         * 
+         * Using this we can easily update the database created already in our existing database
         */
+        public Category category { get; set; }
+        public DateTime? DatePublished { get; set; }
+        public int Level { get; set; }
+
     }
 }
