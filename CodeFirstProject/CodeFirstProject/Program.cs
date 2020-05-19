dbb2874b7bb6e4ef01ef5215fdd2cf0ccbccfdc9
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-19 17:27:34 +0500

    Completed EFC Lecture 31 Seeding Database

--- CodeFirstProject/CodeFirstProject/Program.cs
+++ CodeFirstProject/CodeFirstProject/Program.cs
@@ -4,18 +4,9 @@ using System.Data.Entity;
 
 namespace CodeFirst
 {
-    /* Lecture 30
-     * Migrations: Downgrading a Database
-     * 
-     * two ways of Downgrading the data base
-     * 
-     * 1. Start form version 2
-     * 2. start 1 setp back from version 2
-     * 
-     * 
-     * 
-     * 
-     * update-database -TargitMigration:DeletePublishIdFromCourseColumn
+    /* 
+     * Lecture 31
+     * Seeding Database
      */
 
 
