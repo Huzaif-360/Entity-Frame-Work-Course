bc67c8550a7c55c2dfde9a35944765849cbf5002
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-28 18:23:11 +0500

    Initial Commit EFC Lecture 36 Data Annotations

--- CodeFirstProject/CodeFirstProject/Program.cs
+++ CodeFirstProject/CodeFirstProject/Program.cs
@@ -8,16 +8,8 @@ namespace CodeFirst
      * Section 5 
      * Overriding Code-First Conventions
      * 
-     * Lecture 35 
-     * Overriding Conventions
-     * 1. Data Annotation
-     * 2. Fluent API
-     * 
-     * 
-     * 
-     * 
-     * If you make a mistake make a new mgration and then update the database
-     * Completed Lecture 35
+     * Lecture 36 
+     * Data Annotation
      */
 
 
