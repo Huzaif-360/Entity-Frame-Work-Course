5fa0059fee15b57969319dbf342311cc35e9f3a9
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-28 18:38:21 +0500

    Third Commit EFC Lecture 36 Primary Key

--- CodeFirstProject/CodeFirstProject/Program.cs
+++ CodeFirstProject/CodeFirstProject/Program.cs
@@ -19,6 +19,21 @@ namespace CodeFirst
      *  then table name sould be courses
      *  
      *  If name of the table is different that we should mention it seperately and if it is existing in another schema that we should write the name of the schema also.
+     *  
+     *  
+     *  Name of Columns:
+     *  Name
+     *  ID
+     *  Color
+     *  Class ect........
+     *  
+     *  Same is the case with column as we use with the table name.
+     *  If name of the column is different that we should mention it seperately and if it is existing in another schema that we should write the name of the schema also.
+     *  
+     *  
+     *  Primary Key:
+     *  A property Name ID or ClassName ID
+     *  
      */
 
 
