17c34d2abd7dbd9516dac424cc9b3918a8d865b2
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-29 00:16:28 +0500

    Completed EFC Lecture 38 Fluent API: Basics

--- DataAnnotations/DataAnnotations/Program.cs
+++ DataAnnotations/DataAnnotations/Program.cs
@@ -6,8 +6,24 @@ using System.Threading.Tasks;
 
 
 
-//Lecture 37 
-//Demonstration for Data Annotations is completed 
+//Lecture 38
+//Fluent API
+/* in this case we also do changeings in the name of columns, properties, tables and so on.
+ * 
+ * Table Name
+ * Column Name
+ * Index
+ * Primary Key
+ * Foreign Key
+ * Column Types
+ * Order of Column
+ * DataBase Generated
+ * Length of Stringns
+ * 
+ * 
+ * if a property is nullable and the framework marks that property as nullable in the database as well.
+ */
+
 namespace DataAnnotations
 {
     class Program
