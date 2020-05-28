685ea3525902cf2163ae962a9a886b49da1bd7cc
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-29 01:05:30 +0500

    Completed EFC Lecture 40 Demo: Fluent API

---
+++ FluentAPI/FluentAPI/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Completed Lecture 40:
+//Fluent API
+namespace DataAnnotations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
