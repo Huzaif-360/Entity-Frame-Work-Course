5d253ada37432edd73c4ee2aab52dca90492501f
huzaifa <huzaifa2017@namal.edu.pk>
2020-05-29 18:35:56 +0500

    Completed EFC Lecture 42 Organizing Fluent API Configurations

--- FluentAPI/FluentAPI/EntityConfigurations/CourseConfiguration.cs
+++ FluentAPI/FluentAPI/EntityConfigurations/CourseConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,13 @@ namespace FluentAPI.EntityConfigurations
 {
    public  class CourseConfiguration: EntityTypeConfiguration<course>
     {
-       
+       public CourseConfiguration()
+        {
+            /*
+             * we will put all the data we have for a seperate entity
+             * I mean course is a seperate thing so for this we will use all thing in a seperate class for convinence
+             * in big enterprices we have a lot of codes so we have to manage them as accordingly to get rid of errors
+             */ 
+        }
     }
 }
